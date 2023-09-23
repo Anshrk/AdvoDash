7348bba09c8ededbed0ea9f6227ed0a7ad3c47d8
Ansh Kashyap <anshrk.13@gmail.com>
2023-09-23 19:45:16 +0530

    :coffin: Removed Advodash Ostrich
    Removed the old code base entirely

--- advodash_ostrich/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
-]
--- advodash_ostrich/cases/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.all_cases, name="all cases"),
-    path("add/", views.add_case, name="add"),
-]
